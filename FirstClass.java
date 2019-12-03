1d5af6532c4b9bb5cd209b049d4ba5bb14158950
Erdene-Ochir Mendsaikhan <ts.eric@yahoo.com>
2019-12-03 18:37:45 -0500

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Batch 15");
+        System.out.println("I am still here");
+        System.out.println("I love java");
+
+        System.out.println("I am Erdene here");
+        System.out.println("I am from batch 15");
+        System.out.println("I am from Annandale");
+        System.out.println("I love Java");
+        }
+}
